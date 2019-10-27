d028bd9b1a89e7d3dbd4f6db3057ecb34059a38a
Steve Cotton <steve@octalot.co.uk>
2019-10-27 06:57:45 +0100

    Add other versions of Wesnoth to the Load Game dialog (with fixes from Shadowm)
    If a save directory from another version is found, a drop-down list of
    directories is added next to the filter box in the Load Game dialog.
    
    Refactor save_index_class to allow instances for other directories.
    
    Design idea: all new saves go in to the main save directory. Games can be
    loaded from other directories, but those directories are treated as read-only.
    When building an index for other directories, the index is kept in memory and
    not written to disk - this results in some noticeable lag in the UI when
    opening the other versions' directories, but not the usual save dir.
    
    gui2/game_load: Improve display of old versions selection
    
     * Return paths from fs API in the native format, not the generic
       format. The generic format looks wholly out of place on Windows.
    
     * Put the version selector on the row with the dialog caption and
       Search box as per d53ca602b8df5cc302cd1d5d502654823dc67415.
    
     * Label options to make the purpose of the combobox clearer even
       without the tooltip.
    
     * Fix markup not being taken into account for the combobox option
       labels, and protect against potential markup characters in paths
       (shouldn't happen on Windows but it can happen on POSIX platforms).
    
    Cherry picked from commits b66bdb8a16ec8fbcfffdc14d7a4c4349004f4959,
    666c4d27cef06913af721125929fdd4ef210d90f, and the old-versions part
    of d53ca602b8df5cc302cd1d5d502654823dc67415.

--- src/filesystem_boost.cpp
+++ src/filesystem_boost.cpp
@@ -21,10 +21,10 @@
 
 #include "config.hpp"
 #include "game_config.hpp"
+#include "game_version.hpp"
 #include "log.hpp"
 #include "serialization/unicode.hpp"
 #include "serialization/unicode_cast.hpp"
-#include "game_version.hpp"
 
 #include <boost/algorithm/string.hpp>
 #include <boost/filesystem.hpp>
@@ -527,6 +527,13 @@ std::string get_next_filename(const std::string& name, const std::string& extens
 
 static bfs::path user_data_dir, user_config_dir, cache_dir;
 
+static const std::string get_version_path_suffix(const version_info& version)
+{
+	std::ostringstream s;
+	s << version.major_version() << '.' << version.minor_version();
+	return s.str();
+}
+
 static const std::string& get_version_path_suffix()
 {
 	static std::string suffix;
@@ -535,9 +542,7 @@ static const std::string& get_version_path_suffix()
 	// the version number cannot change during runtime.
 
 	if(suffix.empty()) {
-		std::ostringstream s;
-		s << game_config::wesnoth_version.major_version() << '.' << game_config::wesnoth_version.minor_version();
-		suffix = s.str();
+		suffix = get_version_path_suffix(game_config::wesnoth_version);
 	}
 
 	return suffix;
@@ -802,6 +807,36 @@ std::string get_cache_dir()
 	}
 
 	return cache_dir.string();
+}
+
+std::vector<other_version_dir> find_other_version_saves_dirs()
+{
+	const auto& w_ver = game_config::wesnoth_version;
+	const auto& ms_ver = game_config::min_savegame_version;
+
+	if(w_ver.major_version() != 1 || ms_ver.major_version() != 1) {
+		// Unimplemented, assuming that version 2 won't use WML-based saves
+		return {};
+	}
+
+	std::vector<other_version_dir> result;
+
+	// For 1.16, check for saves from all versions up to 1.20.
+	for(auto minor = ms_ver.minor_version(); minor <= w_ver.minor_version() + 4; ++minor) {
+		if(minor == w_ver.minor_version())
+			continue;
+
+		auto version = version_info{};
+		version.set_major_version(w_ver.major_version());
+		version.set_minor_version(minor);
+		auto suffix = get_version_path_suffix(version);
+		auto path = get_user_data_path().parent_path() / suffix / "saves";
+		if(bfs::exists(path)) {
+			result.emplace_back(suffix, path.string());
+		}
+	}
+
+	return result;
 }
 
 std::string get_cwd()
